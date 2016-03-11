58bd48853805939e767d8ca3a46e8bd35332031a
KnoVz <hknovz@gmail.com>
2016-03-11 14:45:27 +0100

    Remove Grunt from provision Added JsHint, but commented
    I am not using Grunt
    I am using JsHInt, but it will be on devDependencies and be installed locally
    anyway, so it makes no sense to install it globally.

--- provision.sh
+++ provision.sh
@@ -9,7 +9,11 @@ sudo apt-get install -y build-essential
 sudo apt-get install -y git
 # Use file provision to copy ~/.gitconfig
 # Grunt
-sudo npm install -g grunt-cli
+#   Not using grunt
+# sudo npm install -g grunt-cli
+# JsHint
+#   I have it in devDependencies and will be installed locally anyway...
+# sudo npm install -g jshint
 # MongoDb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
